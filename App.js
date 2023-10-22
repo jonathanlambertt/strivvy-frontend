db4ca00bde61500b214f944b78a2bfd46228f16a
Jonathan Lambert <jonathanlambert247@gmail.com>
2023-10-22 16:01:03 -0700

    set up react navigation

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <Text>strivvy</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View>
+        <Text>strivvy</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
