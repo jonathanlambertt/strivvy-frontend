107c8176ef20a3cbdc6dd8031a051d5218617233
jonathan <jonathanlambert247@gmail.com>
2023-11-12 12:14:21 -0800

    add new-post tab

--- src/routes/RootStack.js
+++ src/routes/RootStack.js
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import TabBar from "./TabBar";
+import NewPostScreen from "../screens/NewPostScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,17 @@ const RootStack = () => {
         component={TabBar}
         options={{ headerShown: false }}
       />
+      <Stack.Group
+        screenOptions={{
+          presentation: "modal",
+        }}
+      >
+        <Stack.Screen
+          name="NewPost"
+          component={NewPostScreen}
+          options={{ title: "Share a link" }}
+        />
+      </Stack.Group>
     </Stack.Navigator>
   );
 };
--- src/routes/TabBar.js
+++ src/routes/TabBar.js
@@ -5,31 +5,51 @@ import { AntDesign } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
+const PlaceHolder = () => {};
 
 const TabBar = () => {
   return (
     <Tab.Navigator
-      screenOptions={({ route }) => ({
+      screenOptions={{
         tabBarShowLabel: false,
         tabBarActiveTintColor: "#333",
         tabBarInactiveTintColor: "#a9a9a9",
-        tabBarIcon: ({ color }) => {
-          if (route.name == "Home") {
-            return <AntDesign name="home" size={30} color={color} />;
-          } else if (route.name == "Profile") {
-            return <Feather name="user" size={30} color={color} />;
-          } else if (route.name == "NewPost") {
-            return <Feather name="plus" size={35} color="#333" />;
-          }
-        },
-      })}
+      }}
     >
       <Tab.Screen
         name="Home"
         component={HomeScreen}
-        options={{ title: "strivvy" }}
+        options={{
+          title: "strivvy",
+          tabBarIcon: ({ color }) => {
+            return <AntDesign name="home" size={30} color={color} />;
+          },
+        }}
       />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen
+        name="NewPostTab"
+        component={PlaceHolder}
+        listeners={({ navigation }) => ({
+          tabPress: (e) => {
+            e.preventDefault();
+            navigation.navigate("NewPost");
+          },
+        })}
+        options={{
+          tabBarIcon: () => {
+            return <Feather name="plus-circle" size={35} color="#ed1847" />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color }) => {
+            return <Feather name="user" size={30} color={color} />;
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
