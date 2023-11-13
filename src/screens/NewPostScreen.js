2be8fd3edc080cb26b41bccb04b968a4ab7c4f21
jonathan <jonathanlambert247@gmail.com>
2023-11-12 20:39:22 -0800

    add cancel and share buttons to new-post screen

--- src/screens/NewPostScreen.js
+++ src/screens/NewPostScreen.js
@@ -1,6 +1,22 @@
-import { View } from "react-native";
+import { useEffect } from "react";
+import { View, Button } from "react-native";
 
-const NewPostScreen = () => {
+const NewPostScreen = ({ navigation }) => {
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Button
+          title="Cancel"
+          color={"#333"}
+          onPress={() => navigation.goBack()}
+        />
+      ),
+      headerRight: () => (
+        <Button title="Share" color={"#ed1847"} disabled={true} />
+      ),
+    });
+  });
+
   return <View></View>;
 };
 
