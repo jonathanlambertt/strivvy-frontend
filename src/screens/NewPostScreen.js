768fe87a0e0afce07381029307a36e8cb9ab1b2c
jonathan <jonathanlambert247@gmail.com>
2023-11-15 21:58:06 -0800

    start work on new-post screen

--- src/screens/NewPostScreen.js
+++ src/screens/NewPostScreen.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
-import { View, Button } from "react-native";
+import { Button, ScrollView, TextInput, Text } from "react-native";
+import * as Clipboard from "expo-clipboard";
 
 const NewPostScreen = ({ navigation }) => {
   useEffect(() => {
@@ -17,7 +18,36 @@ const NewPostScreen = ({ navigation }) => {
     });
   });
 
-  return <View></View>;
+  return (
+    <ScrollView style={{ flex: 1, backgroundColor: "#fff", paddingTop: 15 }}>
+      <Text
+        style={{
+          textAlign: "center",
+          color: "#333",
+          fontSize: 16,
+          marginHorizontal: 35,
+        }}
+      >
+        Share links to videos, music, or whatever you love.
+      </Text>
+      <TextInput
+        style={{
+          padding: 10,
+          borderWidth: 1,
+          borderColor: "#a9a9a9",
+          marginHorizontal: 15,
+          borderRadius: 5,
+          height: 48,
+          marginTop: 15,
+          fontSize: 15,
+        }}
+        placeholder="Enter or paste a link"
+        placeholderTextColor={"#a9a9a9"}
+        selectionColor={"#333"}
+        autoFocus
+      />
+    </ScrollView>
+  );
 };
 
 export default NewPostScreen;
